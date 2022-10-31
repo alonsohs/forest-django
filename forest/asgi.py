afffde51492e110b931cdfaf499f303ab04029ef
Alonso Hernández Sastré <alonsohdez14@gmail.com>
2022-10-30 19:11:53 -0600

    Add: Login page

--- forest/asgi.py
+++ forest/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for proyheds project.
+ASGI config for forest project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyheds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forest.settings')
 
 application = get_asgi_application()
--- forest/urls.py
+++ forest/urls.py
@@ -1,4 +1,4 @@
-"""proyheds URL Configuration
+"""forest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/',include('generales.urls')),
     path('forest/',include('catalogos.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
