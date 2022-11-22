b0759477b0a388fb9de7bd00bdf2d424c902d35a
Brenda Vazquez <brendavazquezmimientzi18@outlook.com>
2022-11-22 07:43:55 -0600

    Agregar modelo de proyecto

--- proyectos/models.py
+++ proyectos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Proyectos(models.Model):
+    urlImagen = models.CharField(max_length=150, default='https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png')
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=30)
+    lugar = models.CharField(max_length=30)
+    latitud = models.CharField(max_length=30)
+    altitud = models.CharField(max_length=30)
+    metrosCuadrados = models.CharField(max_length=30)
+   
